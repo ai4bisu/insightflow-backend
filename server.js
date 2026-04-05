50f7d2cf015f6025491c6ecced73ddef053ee6f0
ai4bisu <ai4bisu@gmail.com>
2026-04-06 00:13:07 +0530

    Update server.js
    const PORT = process.env.PORT || 5000;
    
    app.listen(PORT, () => console.log(`Server running on ${PORT}`));

--- server.js
+++ server.js
@@ -76,4 +76,6 @@ app.post("/analyze", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
